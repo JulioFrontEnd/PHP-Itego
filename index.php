8179dc8b3204212b77cd4235f72902bdc7b7c4a6
Julio <juliocesar.sm4100@gmail.com>
2019-09-16 17:12:10 -0300

    Contruindo Interface

--- index.php
+++ index.php
@@ -4,6 +4,20 @@
 	titleMenu($title);
 ?>
 
+<div class="parallax">
+	<div class="after"></div>
+	<div class="title">
+		<h1 class="text-primary">JULIO MOREIRA</h1>
+		<p class="lead text-light value">DESIGNER DE INTERFACES</p>
+		<p class="lead text-light value">DESENVOLVEDOR DE WEBSITES</p>
+		<p class="lead text-light value">FOCO EM FRONT-END</p>
+		<p class="lead text-light value">PLANEJAMENTO DE BANCO DE DADOS</p>
+		<p class="lead text-light value">DESENHISTA</p>
+		<p class="lead text-light value">CONCEPT ARTIST</p>
+	</div>
+</div>
+
+
 
 
 <?php 
