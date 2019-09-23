12e5d86f14fa7a3672ed985654bd0cd9af61716c
Julio <julio.kuros4ki@gmail.com>
2019-09-23 16:55:30 -0300

    Iniciando o gryscale

--- index.php
+++ index.php
@@ -21,10 +21,40 @@
 	</div>
 </div>
 
-<div class="d-flex justify-content-center p-2">
-	<h3>PRINCIPAIS SERVIÇOS 2019 | JULIO UI</h3>
-	<div class="">
-		
+<div class="d-flex p-2 flex-column ">
+	<h3 class="col-12 text-center">PRINCIPAIS SERVIÇOS 2019 | JULIO UI</h3>
+	<div class="col-12 d-flex flex-wrap justify-content-center">
+		<div class="image-bg img-1 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
+
+		<div class="image-bg img-2 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
+
+		<div class="image-bg img-3 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
+
+		<div class="image-bg img-4 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
+
+		<div class="image-bg img-5 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
+
+		<div class="image-bg img-6 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
+
+		<div class="image-bg img-7 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
+
+		<div class="image-bg img-8 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
+			<p class="lead" style="display: none;">WEBSITES</p>
+		</div>
 	</div>
 </div>
 
