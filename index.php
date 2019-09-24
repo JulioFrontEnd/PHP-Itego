c47ed08c671f64de429bbcc61c94189a78b1a4b3
JC <klsdjflkasj@gmail.com>
2019-09-24 16:42:19 -0300

    Luta das transições

--- index.php
+++ index.php
@@ -22,38 +22,38 @@
 </div>
 
 <div class="d-flex p-2 flex-column ">
-	<h3 class="col-12 text-center">PRINCIPAIS SERVIÇOS 2019 | JULIO UI</h3>
+	<h3 class="col-12 text-center" style="color: #2d3436;">PRINCIPAIS SERVIÇOS 2019 | JULIO UI</h3>
 	<div class="col-12 d-flex flex-wrap justify-content-center">
-		<div class="image-bg img-1 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-1 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">DESENHOS</p>
 		</div>
 
-		<div class="image-bg img-2 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-2 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">WEBSITES</p>
 		</div>
 
-		<div class="image-bg img-3 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-3 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">WEBSITES</p>
 		</div>
 
-		<div class="image-bg img-4 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-4 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">WEBSITES</p>
 		</div>
 
-		<div class="image-bg img-5 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-5 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">WEBSITES</p>
 		</div>
 
-		<div class="image-bg img-6 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-6 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">WEBSITES</p>
 		</div>
 
-		<div class="image-bg img-7 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-7 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">WEBSITES</p>
 		</div>
 
-		<div class="image-bg img-8 m-2 rounded" style="width: 300px; height: 300px; background-color: red;" >
-			<p class="lead" style="display: none;">WEBSITES</p>
+		<div class="image-bg img-8 m-2 rounded" style="width: 300px; height: 300px; background-color: #ff7675;" >
+			<p class="lead p-image">WEBSITES</p>
 		</div>
 	</div>
 </div>
